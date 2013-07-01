9988e9947eb173dc544e3c9a9c625e2e62e0c087
Mike Williams <mdub@dogbiscuit.org>
2013-07-01 20:37:36 +1000

    Add more packages.

--- cookbooks/bosh-inception/recipes/packages.rb
+++ cookbooks/bosh-inception/recipes/packages.rb
@@ -1,4 +1,12 @@
 include_recipe "apt"
 
+package "build-essential"
+package "curl"
+package "debootstrap"
+package "genisoimage"
+package "joe"
+package "libsqlite3-dev"
 package "libxml2-dev"
 package "libxslt-dev"
+package "tmux"
+package "vim"
